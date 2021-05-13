f73b2f326bad9482df73cca223d495192093bab1
Trần Đức Thông <102190042@sv1.dut.udn.vn>
2021-05-13 20:14:27 +0700

    commit test git

--- PBLnh2/Program.cs
+++ PBLnh2/Program.cs
@@ -17,6 +17,7 @@ namespace PBLnh2
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            // Test
         }
     }
 }
